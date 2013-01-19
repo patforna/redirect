3ea828c6983ecc5226cfceb6dc86d5b86c1e272c
Patric Fornasier <patric.fornasier@gmail.com>
2013-01-19 21:44:28 +0000

    pat: added redirection and logging.

--- web.rb
+++ web.rb
@@ -4,7 +4,7 @@ require 'json'
 require 'mongo'
 
 configure :development do
-  set :db, Mongo::Connection.new.db('spikes').collection('todos')
+  set :db, Mongo::Connection.new.db('spikes').collection('redirects')
 end
 
 configure :production do
@@ -12,30 +12,17 @@ configure :production do
     db_name = db_uri.path.gsub(/^\//, '')
     db_connection = Mongo::Connection.new(db_uri.host, db_uri.port).db(db_name)
     db_connection.authenticate(db_uri.user, db_uri.password) unless (db_uri.user.nil?)
-    set :db, db_connection.collection('todos')
+    set :db, db_connection.collection('redirects')
 end
 
 get '/' do
-  @todos = settings.db.find.to_a
+  @redirects = settings.db.find.to_a
   erb :index
 end
 
-post '/' do  
-  data = JSON.parse request.body.read  
+get '/redirect' do
+  target = params[:target]
+  data = { :timestamp => Time.now.getutc, :target => target}
   settings.db.insert(data)
-end
-
-put '/:id' do
-  data = JSON.parse request.body.read    
-  settings.db.update({'_id' => bson_id(params[:id])}, {'$set' => data})
-end
-
-delete '/:id' do
-  settings.db.remove(:_id => bson_id(params[:id]))
-end
-
-helpers do
-  def bson_id val
-    BSON::ObjectId.from_string(val)
-  end
+  redirect target
 end
